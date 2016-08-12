17516a12fd085270366d65772ffd40e91cb6f49a
Angel Romero <angel.romero@moonpig.com>
2016-08-12 22:36:38 +0100

    UI test that checks the score on the detail screen for a Manchester or Arsenal match

--- app/src/androidTest/java/com/arao/footballmatches/presentation/view/activity/ClickOnArsenalMatchTest.java
+++ app/src/androidTest/java/com/arao/footballmatches/presentation/view/activity/ClickOnArsenalMatchTest.java
@@ -1,61 +1,129 @@
 package com.arao.footballmatches.presentation.view.activity;
 
-
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.arao.footballmatches.R;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class ClickOnArsenalMatchTest {
 
-    @Rule
-    public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+    private static final int TEAM_1_SCORE_TEXT_VIEW_CHILD_NUMBER_IN_GROUP = 2;
+    private static final int TEAM_2_SCORE_TEXT_VIEW_CHILD_NUMBER_IN_GROUP = 4;
+
+    @Rule public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
 
     @Test
-    public void clickOnArsenalMatchTest() {
-        boolean successfullyClicked = tryToPerformClickOnViewWithText(R.id.team1_name, "Manchester United");
-        if (!successfullyClicked) {
-            successfullyClicked = tryToPerformClickOnViewWithText(R.id.team2_name, "Manchester United");
-            if (!successfullyClicked) {
-                successfullyClicked = tryToPerformClickOnViewWithText(R.id.team1_name, "Arsenal");
-                if (!successfullyClicked) {
-                    successfullyClicked = tryToPerformClickOnViewWithText(R.id.team2_name, "Arsenal");
+    public void shouldHaveSameScoreOnDetailScreenToolbar_whenClickingOnMatchItem() {
+        String score = getScoreForMatchWithTeam(R.id.team1_name, "Manchester United");
+        if (score == null) {
+            score = getScoreForMatchWithTeam(R.id.team2_name, "Manchester United");
+            if (score == null) {
+                score = getScoreForMatchWithTeam(R.id.team1_name, "Arsenal");
+                if (score == null) {
+                    score = getScoreForMatchWithTeam(R.id.team2_name, "Arsenal");
                 }
             }
         }
 
-        if (successfullyClicked) {
-            onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.inner_toolbar))));
+        if (score != null) {
+            String scoreInToolbar = getTextFromViewWithMatcher(
+                    allOf(instanceOf(TextView.class), withId(R.id.collapsed_title)));
+            assertEquals(score, scoreInToolbar);
         } else {
             fail();
         }
     }
 
-    private boolean tryToPerformClickOnViewWithText(int id, String text) {
+    private String getScoreForMatchWithTeam(int id, String text) {
         try {
-            onView(allOf(withId(id), withText(text), isDisplayed())).perform(click());
-            return true;
+            Matcher<View> viewMatcher = allOf(withId(id), withText(text), isDisplayed());
+            String score = getScoreForMatchWithTeamMatcher(viewMatcher);
+            onView(viewMatcher).perform(click());
+            return score;
         } catch (NoMatchingViewException exception) {
-            return false;
+            return null;
         }
+    }
+
+    private String getScoreForMatchWithTeamMatcher(final Matcher<View> matcher) {
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        onView(matcher).perform(new ViewAction() {
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                if (view.getParent() instanceof ViewGroup) {
+                    ViewGroup parent = (ViewGroup) view.getParent();
+
+                    TextView team1ScoreTextView = (TextView) parent.getChildAt(TEAM_1_SCORE_TEXT_VIEW_CHILD_NUMBER_IN_GROUP);
+                    String team1Score = team1ScoreTextView.getText().toString();
+                    TextView team2ScoreTextView = (TextView) parent.getChildAt(TEAM_2_SCORE_TEXT_VIEW_CHILD_NUMBER_IN_GROUP);
+                    String team2Score = team2ScoreTextView.getText().toString();
+
+                    stringBuilder.append(team1Score);
+                    stringBuilder.append(" - ");
+                    stringBuilder.append(team2Score);
+                }
+            }
+        });
+        return stringBuilder.toString();
+    }
+
+    private String getTextFromViewWithMatcher(final Matcher<View> matcher) {
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        onView(matcher).perform(new ViewAction() {
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView tv = (TextView) view;
+                stringBuilder.append(tv.getText().toString());
+            }
+        });
+        return stringBuilder.toString();
     }
 
 }
